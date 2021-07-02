89996ba6aa6e89ac3efd98d7c65b727eb5642d0b
MarioAndrade <yomariolalo@hotmail.com>
2021-07-01 22:58:21 -0500

    Added some fixes

---
+++ src/components/Register/Form.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const LeftImg = () => {
+  return (
+    <Fragment>
+      <div className="rectangle"></div>
+    </Fragment>
+  );
+};
+
+export default LeftImg;
--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,12 +1,18 @@
 import React, { Fragment } from "react";
 
+import LeftImg from "./LeftImg";
+import Form from "./Form";
+
 import "../../styles/Register/styles.scss";
 
 class RegisterCom extends React.Component {
   render() {
     return (
       <Fragment>
-        <div className="bodyR"></div>
+        <div className="">
+          <LeftImg />
+          <Form />
+        </div>
       </Fragment>
     );
   }
