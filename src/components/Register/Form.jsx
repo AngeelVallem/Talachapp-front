4083df5709c75bb90d68bb7b51dbc25c3f014ac2
MarioAndrade <yomariolalo@hotmail.com>
2021-07-07 15:23:51 -0500

    Added register

--- src/components/Register/Form.jsx
+++ src/components/Register/Form.jsx
@@ -1,11 +1,88 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 
-const LeftImg = () => {
+import { useHistory } from "react-router";
+import logo from "../../assets/Home/LOGO.png";
+import { Link } from "react-router-dom";
+
+import CustomInput from "./CustomInput";
+// Services
+// import { postPost } from "../../services";
+
+export default function AddUser() {
+  const [username, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newUser = {
+        username,
+        email,
+        password,
+      };
+      // await postPost(newPost);
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Fragment>
-      <div className="rectangle"></div>
-    </Fragment>
+    <React.Fragment>
+      {/* <div className="containerForm2"> */}
+      <div className="containerForm">
+        <form onSubmit={handleSubmit}>
+          <img className="fLogo3" src={logo} alt="logo-icon" />
+          <label className="lb3">Registro</label>
+          <div className="form-group col-md-6 formDiv">
+            {/* <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title" */}
+            <CustomInput
+              id="Usuario"
+              placeholder="Usuario"
+              value={username}
+              // onChange={(event) => setTitle(event.target.value)}
+              callback={setUser}
+            />
+          </div>
+          <div className="form-group col-md-6 formDiv">
+            <CustomInput
+              id="Correo"
+              placeholder="Correo"
+              value={email}
+              callback={setEmail}
+            />
+          </div>
+          <div className="form-group formDiv">
+            <CustomInput
+              id="Contraseña"
+              type="Contraseña"
+              placeholder="Contraseña"
+              value={password}
+              callback={setPassword}
+            />
+          </div>
+          <button
+            className="btn btn-warning btn-lg btn-block mt-4 buttonStyle"
+            type="submit"
+          >
+            Registrarme
+          </button>
+          <Link className="labLogo3" to="/">
+            <label className="lblMargin">
+              ¿Tienes ya una cuenta? Iniciar Sesión
+            </label>
+          </Link>
+        </form>
+      </div>
+      {/* </div> */}
+    </React.Fragment>
   );
-};
-
-export default LeftImg;
+}
--- src/components/Register/LeftImg.jsx
+++ src/components/Register/LeftImg.jsx
@@ -1,9 +1,18 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Home/LOGO.png";
+import workers from "../../assets/Login/workers.svg";
 
 const LeftImg = () => {
   return (
     <Fragment>
-      <div className="rectangle"></div>
+      <div className="rectangle">
+        <Link className="labLogo2" to="/">
+          <img className="fLogo2" src={logo} alt="logo-icon" />
+          <label>Talachapp</label>
+        </Link>
+        <img className="imgWorkers" src={workers} alt="" />
+      </div>
     </Fragment>
   );
 };
