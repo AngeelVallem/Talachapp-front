ba109093ff3820cbf846110bf03624e3d3fdae4a
Angel Valle <angelvalle@angel.local>
2021-07-13 20:53:48 -0500

    worker card feat

--- src/components/navbar/LoggedNavItems.jsx
+++ src/components/navbar/LoggedNavItems.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {useHistory} from 'react-router-dom'
 import Image from '../Image'
 import Link from '../Link'
 import Button from "../Button";
@@ -9,19 +9,20 @@ import {colors} from "../../globals/index"
 export default function LoggedNavItems(props) {
 	
 	const {user} = props
-
+	const history = useHistory()
 const signOut = () => {
 
 	const {setIsLogged} = props;
 
 	window.localStorage.removeItem('token')
 	setIsLogged(false)
+	history.push('/')
 }
 
   return (
     <div className="d-flex align-items-center logged">	
 	<Button text='signOut' onClick={signOut} color={colors.primary} className='mx-1'/>
-	<p className="p-0">{user.name}{" "}{user.lastName}</p>
+	<p className="p-0 m-0">{user.name}{" "}{user.lastName}</p>
 	<Image src='https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png' circle size='60px'/>
     </div>
   );
